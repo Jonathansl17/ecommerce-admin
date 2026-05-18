432663f865e5eccd6d74f10fa555993b558e20dc
Jonathansl17 <jonathansancho5087@gmail.com>
2026-05-18 10:57:40 -0600

    feat(client-api): orders, users, stock client wrappers

---
+++ backend/shared/clientApi/index.js
@@ -0,0 +1,6 @@
+export * from './client-api.constants.js';
+export * from './client-api.errors.js';
+export * from './client-api.fetch.js';
+export * from './orders.client.js';
+export * from './users.client.js';
+export * from './stock.client.js';
